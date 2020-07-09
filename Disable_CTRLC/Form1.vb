73b9a27c043ff6121c1210d08df16dfedfab1a0b
Roberson Silva <67395473+RobersonSilva@users.noreply.github.com>
2020-07-09 17:39:40 -0300

    Add files via upload

---
+++ Disable_CTRLC/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Enabled = True
+        Timer1.Start()
+        Clipboard.Clear()
+    End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Try
+            Clipboard.Clear()
+        Catch ex As Exception
+            'MsgBox("Ctrl + C é bloqueado! " & vbCrLf & ex.Message)
+        End Try
+    End Sub
+
+End Class
